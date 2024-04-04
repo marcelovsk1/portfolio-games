c959a512c81b17c73b1dd46e63f2262c8aa37a6c
Marcelo Amaral Alves <marceloaa@live.com>
2024-04-03 22:43:51 -0300

    setup

---
+++ games-portfolio/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// import { Helmet } from 'react-helmet';
+import Navbar from "./Components/Navbar/Navbar"
+import './App.css';
+// import Intro from "./Components/Intro/Intro"
+// import About from "./Components/About/About"
+// import Experience from "./Components//Experience/Experience";
+// import Projects from "./Components/Projects/Projects";
+// import Contact from "./Components/Contact/Contact";
+// import Footer from "./Components/Footer/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      {/* <Intro /> */}
+      {/* <About /> */}
+      {/* <Experience /> */}
+      {/* <Projects /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
